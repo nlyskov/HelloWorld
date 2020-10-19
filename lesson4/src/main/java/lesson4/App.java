d09b5c122e4e9b54a2db4cd9b28e0814033ac90e
nlyskov <lyskovn@gmail.com>
2020-10-19 23:37:03 +0300

    lesson4

---
+++ lesson4/src/main/java/lesson4/App.java
@@ -0,0 +1,13 @@
+package lesson4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
